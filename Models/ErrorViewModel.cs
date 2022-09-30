8227cb1f9dc5a0f2f580928b253e1c63f787a7fe
elmer <elmer@ELMERJESPANA>
2022-09-30 10:29:39 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cursodotcore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
